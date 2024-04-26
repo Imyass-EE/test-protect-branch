2301791c486a9574e2a74c033781b9fd56c84160
Ilyass Elkhadiri <97830506+imyass59@users.noreply.github.com>
2024-04-26 20:59:29 +0100

    Update package-lock.json and package.json with updated supertest types

---
+++ tests/controllers/IndexController.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest"
+import app, { server } from "../../src/index"
+
+describe("IndexController", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "log").mockImplementation(() => {})
+    jest.resetModules()
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
+  it("should return status 200 and 'Test App: API Service' message", async () => {
+    const response = await request(app).get("/")
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      status: "success",
+      message: "Test App: API Service",
+    })
+  })
+})
